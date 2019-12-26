5bc7c05b350322d46ba6dc227622985adb5fe165
GLOOMY <liuzhenm@mail.ustc.edu.cn>
2019-12-27 01:10:08 +0800

    add the new progress view which make user more comfortable

--- main.cc
+++ main.cc
@@ -1,11 +1,26 @@
 #include "buffer/BMgr.h"
 int main() {
-     FILE *test_file = fopen("../tests/data-5w-50w-zipf.txt","rw");
+    FILE *test_file = fopen("../tests/data-5w-50w-zipf.txt","rw");
     int request_type,request_page;
     auto bmgr = new BufferManager::BMgr();
+    int operation_count = 0;
     while(fscanf(test_file,"%d,%d\n",&request_type,&request_page) ==2){
         bmgr->FixPage(request_page,request_type);
+	operation_count += 1;
+	if(operation_count %100 == 0){
+	    printf("\33[2K\r");
+	    printf("Running Progress:%3d%% ....                   [",operation_count*100/500000);
+	    for(int i = 0; i < 20*operation_count/500000;i++)
+		printf(">");
+	    for(int i = 0; i < 20- 20*operation_count/500000;i++)
+		printf("-");
+	    printf("]");
+	    if( operation_count == 500000)
+		printf("comlete!");
+	   fflush(stdout);
+	}
     }
+    printf("\n");
     delete bmgr;
 
     return 0;
